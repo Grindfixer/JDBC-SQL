89773f1a60dcce58f5472b24ebe2e1786df6080b
jwn812 <john.nicholson812@gmail.com>
2019-06-14 19:55:35 -0400

    Activities only, needs backend code

---
+++ app/src/main/java/com/jwn/jdbc_sql/Dbstrings.java
@@ -0,0 +1,9 @@
+package com.jwn.jdbc_sql;
+
+public class Dbstrings {
+
+    static final String DATABASE_URL = "127.0.0.2:3306";
+    static final String DATABASE_NAME = "fruit";
+    static final String USERNAME = "root";
+    static final String PASSWORD = "pineywatermelon";
+}
